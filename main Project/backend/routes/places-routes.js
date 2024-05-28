d2b422571b34e23070859ddffb860774f0704cbf
Asher Etherington <asherce21@gmail.com>
2024-05-28 14:07:59 -0700

    Update places-routes.js

--- main Project/backend/routes/places-routes.js
+++ main Project/backend/routes/places-routes.js
@@ -13,8 +13,21 @@ const DUMMY_PLACES = [
         },
         address: '22955 sw hosler way, sherwood, OR',
         creator: 'u1'
+    }, 
+    {
+        id: 'p2',
+        title: 'house 2',
+        description: 'my house 2',
+        location: {
+            lat: 45.35424919832371,
+            lng: -122.86317397425815
+        },
+        address: '22955 sw hosler way, sherwood, OR',
+        creator: 'u1'
     }
-]//returns a single place with matching id
+]
+
+//gets a single place with matching id
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(place => {
@@ -24,6 +37,20 @@ router.get('/:pid', (req, res, next) => {
         res.json({place});
     } else{
         return res.json({message: "no place found"})
+    }
+});
+
+//gets an array of places associated with a user id
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const usersPlaces = DUMMY_PLACES.filter((place) => {
+        return place.creator === userId;
+    });
+
+    if (usersPlaces) {
+        res.json({usersPlaces});
+    } else{
+        return res.json({message: "no places found for user"})
     }
 });
 
